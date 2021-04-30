3fe413a6b04cbfaca23efb48bd1f60fdd467afa4
jarmeister99 <jarmeister99@gmail.com>
2021-04-30 10:21:39 -0700

    added requirements.txt and basic pymongo framework

---
+++ app/models/users.py
@@ -0,0 +1,19 @@
+
+import pymongo
+from app.models.model import Model
+
+class User(Model):
+    db_client = pymongo.MongoClient('localhost', 27017)
+    collection = db_client["users"]["users_list"]
+
+    def find_all(self):
+        users = list(self.collection.find())
+        for user in users:
+            user["_id"] = str(user["_id"])
+        return users
+
+    def find_by_name(self, username):
+        users = list(self.collection.find({"username": name}))
+        for user in users:
+            user["_id"] = str(user["_id"])
+        return users
