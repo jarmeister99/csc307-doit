46dff63c6af84b264752fd2ab84faf3b92e08649
jarmeister99 <jarmeister99@gmail.com>
2021-05-01 10:39:09 -0700

    implemented flask-login user loader

--- app/models/users.py
+++ app/models/users.py
@@ -6,32 +6,31 @@ db = mongo.db
 
 
 class User():
-    pass
+    def add_user(self, username: str, password_hash: int, email: str):
+        user = {
+            'username': username,
+            'password_hash': password_hash,
+            'email': email
+        }
+        if (db['users'].count_documents({'username': username}) == 0):
+            db['users'].insert_one(user)
+            return True
+        else:
+            return False
+    def validate_password(self, username: str, password_hash: int):
+        user = {
+            'username': username,
+            'password_hash': password_hash
+        }
+        if (db['users'].count_documents({'username': username, 'password_hash': password_hash}) == 1):
+            return True
+        else:
+            return False
 
 @login_manager.user_loader
 def load_user(user_id):
     return db['users'].find_one({'_id': ObjectId(id)})
 
-def validate_password(username: str, password_hash: int):
-    user = {
-        'username': username,
-        'password_hash': password_hash
-    }
-    if (db['users'].count_documents({'username': username, 'password_hash': password_hash}) == 1):
-        return True
-    else:
-        return False
 
 
-def add_user(username: str, password_hash: int, email: str):
-    user = {
-        'username': username,
-        'password_hash': password_hash,
-        'email': email
-    }
-    if (db['users'].count_documents({'username': username}) == 0):
-        db['users'].insert_one(user)
-        return True
-    else:
-        return False
 
--- app/routes/user_routes.py
+++ app/routes/user_routes.py
@@ -1,4 +1,4 @@
-from app.models.users import add_user, validate_password
+from app.models.users import User
 from flask.globals import current_app as app
 from flask import request
 
@@ -11,7 +11,7 @@ def login_route():
             return {}, 400
         if not set(request.json.keys()).issubset({'username', 'password_hash'}):
             return {}, 400
-        if not validate_password(request.json['username'], request.json['password_hash']):
+        if not User().validate_password(request.json['username'], request.json['password_hash']):
             return {}, 401
         return {}, 200
     return {}, 202
@@ -23,7 +23,7 @@ def register_route():
             return {}, 400
         if not set(request.json.keys()).issubset({'username', 'password_hash', 'email'}):
             return {}, 400
-        if not add_user(request.json['username'], request.json['password_hash'], request.json['email']):
+        if not User().add_user(request.json['username'], request.json['password_hash'], request.json['email']):
             return {}, 403
         return {}, 201
     return {}, 202
