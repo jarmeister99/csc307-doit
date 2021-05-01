67398337f91127d6450d15597310434eca528254
jarmeister99 <jarmeister99@gmail.com>
2021-05-01 10:25:05 -0700

    added secret key

--- app/__init__.py
+++ app/__init__.py
@@ -4,16 +4,26 @@ sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 from flask import Flask
 from flask_cors import CORS
 
+import secret
+
 from app.database import mongo
+from app.login import login_manager
 
 # factory design pattern
 def create_app(db_uri: str):
     app = Flask(__name__)
+
+    app.secret_key = secret.secret_key
+
+    # cross origin resource sharing (React <-> Flask)
     CORS(app)
 
     # initialize mongodb
     app.config['MONGO_URI'] = db_uri
     mongo.init_app(app)
+
+    # initialize login manager
+    login_manager.init_app(app)
 
     # ... here are all the pieces of our program
     with app.app_context():
