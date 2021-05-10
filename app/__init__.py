cae973763e7825b699b946acf9eae04e8ae211ae
jarmeister99 <jarmeister99@gmail.com>
2021-05-10 09:10:36 -0700

    removed the ability to run __init__.py, please use flask run instead

--- app/__init__.py
+++ app/__init__.py
@@ -33,8 +33,8 @@ def create_app(db_uri: str):
 
         return app
 
-if __name__ == '__main__':
-    app = create_app('mongodb://localhost:27017/doit')
-    app.run()
+# if __name__ == '__main__':
+#     app = create_app('mongodb://localhost:27017/doit')
+#     app.run()
 
 
