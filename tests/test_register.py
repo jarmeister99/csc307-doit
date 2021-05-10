27fe47e417165edd9d73eb7cd771f70589bf4942
jarmeister99 <jarmeister99@gmail.com>
2021-05-10 09:41:48 -0700

    changed assert statements to NOT use parantheses (to pass lint)

--- tests/test_register.py
+++ tests/test_register.py
@@ -5,18 +5,18 @@ import pytest
 
 def test_register_no_data(client):
     resp = client.post('/register', json={})
-    assert(resp.status_code == 400) # bad request
+    assert resp.status_code == 400 # bad request
 
 def test_register_incorrect_data(client):
     data = {'dont_care': 7, 'not_the_right_data': 10}
     resp = client.post('/register', json=data, content_type='application/json')
-    assert(resp.status_code == 400) # bad request 
+    assert resp.status_code == 400 # bad request 
 
 def test_register_username_success(client, db):
     data = {'username': 'jared', 'password_hash': 0xABCD, 'email': 'email@address.com'}
     resp = client.post('/register', json=data, content_type='application/json')
-    assert(resp.status_code == 201) # resource created
-    assert(db['users'].count_documents({'username': 'jared'}) == 1) # resource present in database
+    assert resp.status_code == 201 # resource created
+    assert db['users'].count_documents({'username': 'jared'}) == 1 # resource present in database
 
 def test_register_username_taken(client, db):
     data = {'username': 'taken', 'password_hash': 0xABCD, 'email': 'email@address.com'}
@@ -27,8 +27,8 @@ def test_register_username_taken(client, db):
     resp = client.post('/register', json=data, content_type='application/json')
     l2 = db['users'].count_documents({})
 
-    assert(resp.status_code == 403) # forbidden request
-    assert(l1 == l2) # collection length didnt change
+    assert resp.status_code == 403 # forbidden request
+    assert l1 == l2 # collection length didnt change
 
 
 
