9adcaea6efebd5755950e73719059906de222887
Hunter Waite <hunterwaite@Hunters-MacBook-Air.local>
2021-05-14 12:14:29 -0700

    Added test of MyApp for continuous integration purposes

---
+++ src/MyApp.test.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import MyApp from './MyApp';
+
+it('renders without crashing', () => {
+  const div = document.createElement('div');
+  ReactDOM.render(<MyApp />, div);
+});
