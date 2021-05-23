79a78c7adc542c64937a9c5f4724caca7e8aaaf0
jarmeister99 <jarmeister99@gmail.com>
2021-05-23 09:17:24 -0700

    linted out test_tasks.py

--- tests/test_register.py
+++ tests/test_register.py
@@ -10,7 +10,7 @@ def test_register_no_data(client):
 def test_register_incorrect_data(client):
     data = {'dont_care': 7, 'not_the_right_data': 10}
     resp = client.post('/register', json=data, content_type='application/json')
-    assert resp.status_code == 400 # bad request 
+    assert resp.status_code == 400 # bad request
 
 def test_register_username_success(client, db):
     data = {'username': 'jared', 'password_hash': 0xABCD, 'email': 'email@address.com'}
@@ -29,7 +29,3 @@ def test_register_username_taken(client, db):
 
     assert resp.status_code == 403 # forbidden request
     assert l1 == l2 # collection length didnt change
-
-
-
-
--- tests/test_tasks.py
+++ tests/test_tasks.py
@@ -1,35 +1,52 @@
-import os, sys 
+"""
+These testcases cover the /tasks route which is responsible for creating, modifying, deleting,
+and retrieving todo-list tasks
+"""
+
+import os
+import sys
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
-import pytest
-
 def test_create_task_no_data(client):
+    """
+    This testcase tests the /tasks POST route with an empty payload
+    """
     resp = client.post('/tasks',json={})
     assert resp.status_code == 400 # bad request
 
 def test_create_task_success(client, db):
+    """
+    This testcase tests the /tasks POST route with a correct payload
+    """
     data = {'name': 'walk the dog', 'description': '', 'dueTime': None}
     resp = client.post('/tasks',json=data)
     assert resp.status_code == 201 # task created successfully
     assert db['tasks'].count_documents({'name': 'walk the dog'}) == 1 #task present in database
 
 def test_create_task_no_name(client, db):
+    """
+    This testcase tests the /tasks POST route without providing a name
+    """
     data = {'name': '', 'description': 'walk the dog', 'dueTime': None}
     resp = client.post('/tasks',json=data)
     assert resp.status_code == 400 # bad request
     assert db['tasks'].count_documents({'name': 'walk the dog'}) == 0 #task not present in database
 
 def test_create_task_bad_data(client):
+    """
+    This testcase tests the /tasks POST route with an incorrect payload
+    """
     data = {'not': 'important', 'a': 'b', 'c': 'd'}
     resp = client.post('/tasks',json=data)
     assert resp.status_code == 400 # bad request
 
-def test_get_two_tasks(client,db):
+def test_get_two_tasks(client, db):
+    """
+    This testcase tests the /tasks GET route with two entries in th edb
+    """
     data = {'name': 'walk the dog', 'description': '', 'dueTime': None}
     data2 = {'name': 'do the laundry', 'description': '', 'dueTime': None}
     client.post('/tasks',json=data)
     client.post('/tasks',json=data2)
     resp = client.get('/tasks')
     assert resp.status_code == 200 # tasks were gotten
-
-
