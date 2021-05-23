90ec43b0d0e855e45ec7d75510d2781526c3de4f
jarmeister99 <jarmeister99@gmail.com>
2021-05-23 09:22:08 -0700

    linted out test_register.py

--- tests/test_register.py
+++ tests/test_register.py
@@ -1,24 +1,39 @@
-import os, sys
+"""
+These testcases cover the /register route which is responsible for creation of new users
+"""
+
+import os
+import sys
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
-import pytest
-
 def test_register_no_data(client):
+    """
+    This testcase covers the /register route with no payload
+    """
     resp = client.post('/register', json={})
     assert resp.status_code == 400 # bad request
 
 def test_register_incorrect_data(client):
+    """
+    This testcase covers the /register route with an incorrect payload
+    """
     data = {'dont_care': 7, 'not_the_right_data': 10}
     resp = client.post('/register', json=data, content_type='application/json')
     assert resp.status_code == 400 # bad request
 
 def test_register_username_success(client, db):
+    """
+    This testcase covers the /register route with a correct payload and available username
+    """
     data = {'username': 'jared', 'password_hash': 0xABCD, 'email': 'email@address.com'}
     resp = client.post('/register', json=data, content_type='application/json')
     assert resp.status_code == 201 # resource created
     assert db['users'].count_documents({'username': 'jared'}) == 1 # resource present in database
 
 def test_register_username_taken(client, db):
+    """
+    This testcase covers the /register route with a correct payload and unavailble username
+    """
     data = {'username': 'taken', 'password_hash': 0xABCD, 'email': 'email@address.com'}
     client.post('/register', json=data, content_type='application/json')
     l1 = db['users'].count_documents({})
