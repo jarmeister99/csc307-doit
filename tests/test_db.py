b765724b745ade046e6c7cd8e0dbdd9212374491
jarmeister99 <jarmeister99@gmail.com>
2021-05-23 09:06:10 -0700

    linted out test_login.py, test_db.py

--- tests/test_db.py
+++ tests/test_db.py
@@ -1,8 +1,15 @@
+"""
+These testcases cover the db fixture used for testing
+"""
+
 import os
 import sys
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
 def test_db_insertion(db):
+    """
+    This testcase ensures that users can be inserted and counted in the test database
+    """
     db['users'].insert_one({'name': 'jared'})
     assert db['users'].count_documents({}) == 1
 
@@ -10,5 +17,8 @@ def test_db_insertion(db):
     assert db['todos'].count_documents({}) == 1
 
 def test_empty_db(db):
+    """
+    This tetscase ensures that the fixture db resets its contents on every use
+    """
     assert db['users'].count_documents({}) == 0
     assert db['todos'].count_documents({}) == 0
--- tests/test_login.py
+++ tests/test_login.py
@@ -1,24 +1,41 @@
-import os, sys
+"""
+These testcases cover the /login route which is responsible for authenticating users.
+"""
+
+import os
+import sys
+
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
-from app.models.users import User
 from flask_login import current_user
 
 def test_login_no_data(client):
+    """
+    This testcase tests the /login route with an empty payload
+    """
     resp = client.post('/login', json={})
     assert resp.status_code == 400 # bad request
 
 def test_login_incorrect_data(client):
+    """
+    This testcase tests the /login route with an incorrect payload
+    """
     data = {'dont_care': 7, 'not_the_right_data': 10}
     resp = client.post('/login', json=data, content_type='application/json')
     assert resp.status_code == 400 # bad request
 
 def test_login_missing_password(client):
+    """
+    This testcase tests the /login route with a missing password
+    """
     data={'username': 'jared'}
     resp = client.post('/login', json=data, content_type='application/json') # log in user
     assert resp.status_code == 400 # bad request
 
 def test_login_correct_password(client, db):
+    """
+    This testcase tests the /login route with correct credentials
+    """
     data = {'username': 'jared', 'password_hash': 0xABCD, 'email': 'email@address.com'}
     resp = client.post('/register', json=data, content_type='application/json') # create user
     assert not current_user.is_authenticated
@@ -31,6 +48,9 @@ def test_login_correct_password(client, db):
     assert current_user.username == 'jared' # ensure correct user was logged in
 
 def test_login_incorrect_password(client, db):
+    """
+    This testcase tests the /login route with an incorrect password
+    """
     data = {'username': 'jared', 'password_hash': 0xABCD, 'email': 'email@address.com'}
     resp = client.post('/register', json=data, content_type='application/json')
     assert not current_user.is_authenticated
@@ -42,6 +62,9 @@ def test_login_incorrect_password(client, db):
     assert not current_user.is_authenticated
 
 def test_login_nonexistent_user(client, db):
+    """
+    This testcase tests the /login route with a nonexistent user
+    """
     data={'username': 'jared', 'password_hash': 0xFFFF}
     resp = client.post('/login', json=data, content_type='application/json')
     assert resp.status_code == 401
