bfaa900e05e995192bfdb2315e9f41f0a793ce29
Hunter Waite <hunterwaite@Hunters-MacBook-Air.local>
2021-06-01 15:11:43 -0700

    linked frontend and backend

--- src/components/Tasks.jsx
+++ src/components/Tasks.jsx
@@ -1,20 +1,19 @@
-import React, {useState} from "react";
+import React, {useState, useEffect} from "react";
 import "../index.css"
 import "./css/Tasks.css"
+import axios from 'axios';
+
 
 function Tasks() {
     // tasks API not implemented yet but will return JSON in this form for future reference
     // {{id_1, title_1, due_data_1, ...}, {id_2, title_2, due_date_2, ...}, {id_3, title_3, due_data_3, ...}}
-    const [tasks, setTasks] = useState([
-        { id: 1, title: 'task_1', due_date: '01/12/2021', completed: true },
-        { id: 2, title: 'task_2', due_date: '01/12/2021', completed: false },
-        { id: 3, title: 'task_3', due_date: '01/12/2021', completed: true },
-        { id: 4, title: 'task_4', due_date: '01/12/2021', completed: false },
-        { id: 5, title: 'task_5', due_date: '01/12/2021', completed: false },
-        { id: 5, title: 'task_7', due_date: '01/12/2021', completed: false },
-        { id: 5, title: 'task_8', due_date: '01/12/2021', completed: false },
-        { id: 5, title: 'task_9', due_date: '01/12/2021', completed: false }
-    ]);
+    const [tasks, setTasks] = useState([ ]);
+        useEffect(() => {
+            fetchAll().then( result => {
+               if (result)
+                  setTasks(result);
+             });
+    }, [] );
 
     // Returns a conditional render of the completed status of a task
     function renderCompleted(completed) {
@@ -24,6 +23,18 @@ function Tasks() {
           return <td>No</td>;
         }
     }
+
+    async function fetchAll(){
+        try {
+           const response = await axios.get('http://localhost:5000/tasks', {withCredentials: true});
+           return response.data;
+        }
+        catch (error){
+           //We're not handling errors. Just logging into the console.
+           console.log(error); 
+           return false;         
+        }
+     }
 
     return (
         <div className="container">
@@ -42,8 +53,8 @@ function Tasks() {
                 <tbody>
                     {tasks && tasks.map(task =>
                         <tr key={task.id}>
-                            <td>{task.title}</td>
-                            <td>{task.due_date}</td>
+                            <td>{task.name}</td>
+                            <td>{task.dueTime}</td>
                             {renderCompleted(task.completed)}
                             <td><a href="http://localhost:3000/edittask"><button className="buttons" >Edit Task</button></a></td>
                             <td><a href="http://localhost:3000/deletetask"><button className="buttons" >Delete Task</button></a></td>
