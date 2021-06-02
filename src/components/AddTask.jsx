95f540f4151ad14c49fdbe00e14aa0d60e625dcb
Coldice54 <alex@berkaloff.com>
2021-06-01 18:30:27 -0700

    Update AddTask.jsx

--- src/components/AddTask.jsx
+++ src/components/AddTask.jsx
@@ -1,41 +1,39 @@
-import React, {useState} from "react";
-import "../index.css"
-import "./css/AddTask.css"
+import React, { useState } from "react";
+import DateTimePicker from 'react-datetime-picker';
 import axios from 'axios';
+import "../index.css"
+import "./css/EditTask.css"
 
 function AddTask() {
     const [task, setTask] = useState(
         {
             name: '',
             description: '',
-            dueTime: ''
+            dueTime: new Date()
         }
     )
     function handleChange(event) {
         const { name, value } = event.target;
         console.log("handlechange")
         if (name === "taskdescription")
-        setTask(
-            {name: task.name, description: value, dueTime: task.dueTime}
-        );
-        else if (name === "taskname")
-        setTask(
-            {name: value, description: task.description, dueTime: task.dueTime}   
-        );
-    }
-    async function submitTask(){
-        try
-        {
             setTask(
-                {name: task.name, description: task.description, dueTime: '06.27.1999'}   
+                { name: task.name, description: value }
             );
-            console.log(task);
-            const response = await axios.post('http://localhost:5000/tasks', task,{withCredentials: true})
+        else if (name === "taskname")
+            setTask(
+                { name: value, description: task.description }
+            );
+    }
+    function dateChange(date) {
+        setTask({ name: task.name, description: task.description, dueTime: date })
+    }
+    async function submitTask() {
+        try {
+            const response = await axios.post('http://localhost:5000/tasks', task, { withCredentials: true })
             console.log("task successfully submitted")
             return response
         }
-        catch(error)
-        {
+        catch (error) {
             console.log(error)
             console.log("error while submitting task")
             return false
@@ -44,7 +42,25 @@ function AddTask() {
     return (
         <div className="col-md-3">
             <form>
-            <div className="taskname">
+                <div className="dt">
+                    <DateTimePicker
+                        name="dt"
+                        amPmAriaLabel="Select AM/PM"
+                        calendarAriaLabel="Toggle calendar"
+                        clearAriaLabel="Clear value"
+                        dayAriaLabel="Day"
+                        hourAriaLabel="Hour"
+                        maxDetail="minute"
+                        minuteAriaLabel="Minute"
+                        monthAriaLabel="Month"
+                        nativeInputAriaLabel="Date and time"
+                        onChange={dateChange}
+                        secondAriaLabel="Second"
+                        value={task.dueTime}
+                        yearAriaLabel="Year"
+                    />
+                </div>
+                <div className="taskname">
                     <label htmlFor="Task Name">Task Name ‎‏‏‎ ‎‏‏‎ ‎</label>
                     <input
                         type="text"
@@ -53,29 +69,35 @@ function AddTask() {
                         class="tbox"
                         value={task.name}
                         onChange={handleChange}
-                        />
-            </div>
-            <div className="taskdescription">
-                <label htmlFor="taskdescription">Task Description ‎‏‏‎‎‏‏‎ ‎‏‏‎ ‎</label>
-                <textarea
-                    type="text"
-                    name="taskdescription"
-                    id="taskdescription"
-                    class="descriptiontbox"
-                    value={task.description}
-                    onChange={handleChange} 
                     />
-            </div>
-            <div className="submitbutton">
-                <input 
-                    type="button" 
-                    class="button" 
-                    value="Add Task" 
-                    onClick={submitTask} 
+                </div>
+                <div className="taskdescription">
+                    <label htmlFor="taskdescription">Task Description ‎‏‏‎‎‏‏‎ ‎‏‏‎ ‎</label>
+                    <textarea
+                        type="text"
+                        name="taskdescription"
+                        id="taskdescription"
+                        class="descriptiontbox"
+                        value={task.description}
+                        onChange={handleChange}
                     />
-            </div>    
+                </div>
+                <div className="submitbutton">
+                    <input
+                        type="button"
+                        class="button"
+                        value="Add Task"
+                        onClick={submitTask}
+                    />
+                </div>
+
             </form>
+
+
+
         </div>
+
+
     );
 
 
